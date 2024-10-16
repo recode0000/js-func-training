f87cecb8a2d9611ac960114b8f56909f6dae3994
ikeda <rikito1036@icloud.com>
2024-10-17 06:31:24 +0900

    update: RegExp オブジェクト確認

--- regexp.js
+++ regexp.js
@@ -1,3 +1,12 @@
-function checkRegExpObject() {}
+function checkRegExpObject() {
+  const pattern = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/gi;
+  let str = "テストURLはhttps://www.wings.msn.to/です。";
+  str += "その次はHTTP://www.web-deli.com/もよろしく！";
+  console.log(str);
+  //matchメソッドは正規表現パターンにマッチした文字列を配列として返す。マッチしない場合はnullを返す
+  const result = str.match(pattern);
+  console.log(result);
+  console.log(pattern.test(str)); // マッチングの有無を返すテストメソッド
+}
 
 checkRegExpObject();
